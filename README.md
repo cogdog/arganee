1e3c21c989958cc55b46f38d8a0f38bd2b5fa291
CogDog <cogdogblog@gmail.com>
2021-02-28 12:17:20 -0600

    Footer Yeah, baby

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Arganee Wordpress Theme
 
-A [SPLOT](http://splot.ca/) for building collections of audio, video, and image media, kind of like a jukebox (you do know what that is, right?).
-
 by Alan Levine https://cog.dog or http://cogdogblog.com/
 
 -----
@@ -21,8 +19,12 @@ A Wordpress theme hacked out of the [HTML5up Eventually Theme](http://html5up.ne
 
 This was done following [envato tuts+ Creating a WordPress Theme From Static HTML: Preparing the Markup](https://code.tutsplus.com/tutorials/creating-a-wordpress-theme-from-static-html-preparing-the-markup--wp-33895) The dynamic content is generated from the titles of "posts" using the Wordpress API to load via JavaScript (thanks to a hand from [Tom Woodward](http://bionicteaching.com/).
 
+More details here [Hacking a WordPress Theme from HTML5up Template with some API goodies](https://cogdogblog.com/2017/01/wp-theme-html5up-template/)
+
 ## Ch-ch-ch-changes
 
+* Feb 28, 2021 - Footer clean up, ought to make menus soon WordPress driven rather than hard coded into footer, also consider a means to contribute new quotes in SPLOT like fashion.
 * Feb 1, 2021 - fixed heading typo and set up better CSS to anchor footer in bottom
+* Jan 17, 2017 - first slapped together and sent into orbit
 
 
--- footer.php
+++ footer.php
@@ -1,25 +1,26 @@
 			</div><!-- .main -->
 
 		<!-- Footer -->
+		<!-- yikes these are hard wired, need to integrate a menu method -->
 			<footer id="footer">
 				<div class="fatfooter">
 					<ul class="icons">
 						<li><a href="https://twitter.com/netnarr" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
 						<li><a href="https://twitter.com/hashtag/netnarr?f=tweets&vertical=default&src=hash" class="icon fa-hashtag"><span class="label">#netnarr</span></a></li>
 						<li><a href="/about" class="icon fa-info"><span class="label">Info</span></a></li>
-						<li><a href="#" class="icon fa-cogs"><span class="label">Cogs</span></a></li>
-						<li><a href="#" class="icon fa-fire"><span class="label">Fire</span></a></li>
-						<li><a href="#" class="icon fa-university"><span class="label">Uni</span></a></li>
-						<li><a href="#" class="icon fa-paint-brush"><span class="label">paint</span></a></li>
-						<li><a href="#" class="icon fa-random"><span class="label">Random</span></a></li>
-						<li><a href="#" class="icon fa-thermometer-half"><span class="label">Twitter</span></a></li>
+						<li><a href="https://cog.dog/" class="icon fa-cogs"><span class="label">Cogs</span></a></li>
+						<li><a href="http://journal.arganee.world/" class="icon fa-fire"><span class="label">Fire</span></a></li>
+						<li><a href="http://kean.edu/" class="icon fa-university"><span class="label">Uni</span></a></li>
+						<li><a href="http://make.arganee.world/" class="icon fa-paint-brush"><span class="label">paint</span></a></li>
+						<li><a href="http://whisper.arganee.world/" class="icon fa-random"><span class="label">Random</span></a></li>
+						<li><a href="http://twitter.com/netnarr" class="icon fa-thermometer-half"><span class="label">Twitter</span></a></li>
 						
-						<li><a href="#" class="icon fa-rocket"><span class="label">Rocket</span></a></li>
+						<li><a href="http://netnarr.arganee.world/" class="icon fa-rocket"><span class="label">Rocket</span></a></li>
 					
 					</ul>
 					
 					<ul class="copyright">
-						<li>Eventually theme (<a href="https://html5up.net/license">CC-BY</a>) from <a href="https://html5up.net">HTML5 UP</a> &bull; Wordpressified by <a href="https://cog.dog/" target="_blank">cog.dog</a></li> 
+						<li><a href="https://github.com/cogdog/arganee" target="_blank">arganee theme</a>  &bull; built atop <a href="https://html5up.net/eventually" target="_blank">Eventually by HTML5UP</a> &bull; Wordpressified by <a href="https://cog.dog/" target="_blank">cog.dog</a></li> 
 					</ul>
 					
 				</div>
